2b0e72a091de7503d8d09e5782734eff2c0a5075
Limboi Sergiu <sergiu_george2007@yahoo.com>
2018-01-28 20:44:04 +0200

    Add Pearson Correlation

---
+++ Statistical-tests/Tests/PearsonCorrelation.py
@@ -0,0 +1,20 @@
+from scipy.stats import pearsonr
+
+"""
+Computes the Pearson Correlation Coefficient
+Params: x,y, -variables for which it is computed the strength of their linear association
+Return : r-pearson correlation
+         p - p value
+"""
+def computePearson(x,y):
+    r,p = pearsonr(x,y)
+    return r,p
+
+def testPearson():
+    age = [43,21,25,42,57,59]
+    glucoseLevel = [99,65,79,75,87,81]
+    r,p = computePearson(age,glucoseLevel)
+    print("Pearson correlation value:",r)
+    print("p-value:",p)
+
+testPearson()
